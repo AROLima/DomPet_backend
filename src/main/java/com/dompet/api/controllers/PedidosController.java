400245c2ca9a6a0e8d5c6c89e60b44cdf6f102f0
Anderson Lima <arolima546@gmail.com>
2025-07-11 18:42:18 -0300

    add repo to controller

--- src/main/java/com/dompet/api/controllers/PedidosController.java
+++ src/main/java/com/dompet/api/controllers/PedidosController.java
@@ -1,8 +1,12 @@
 package com.dompet.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dompet.api.models.pedidos.PedidosRepository;
 
 @RestController
 public class PedidosController {
-    
+    @Autowired
+    private PedidosRepository repository;
 }
--- src/main/java/com/dompet/api/controllers/ProdutosController.java
+++ src/main/java/com/dompet/api/controllers/ProdutosController.java
@@ -1,10 +1,14 @@
 package com.dompet.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dompet.api.models.produtos.ProdutosRepository;
 
 @RestController
 
 public class ProdutosController {
     
-
+    @Autowired 
+    private ProdutosRepository repository;
 }
