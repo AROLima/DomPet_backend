fab0f842549988a1f3ff2dc12ea3250346bd1250
Rodrigo Lima <Rodrigo.lima546@hotmail.com>
2025-09-08 18:09:26 -0300

    static removed

--- src/main/resources/static/flutter_bootstrap.js
+++
@@ -1,4 +0,0 @@
-// (Mantido minificado conforme build original)
-if (!window._flutter) { window._flutter = {}; }
-_flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a5767","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
-_flutter.loader.load({ serviceWorkerSettings: { serviceWorkerVersion: "1105660469" } });
